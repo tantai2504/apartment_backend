bceb9f7d010bbd101e74638c9e69d06f430aa1b1
MiiNorin <hodangdxb@gmail.com>
2025-03-16 14:11:47 +0700

    fix application & configuration

--- src/main/java/com/example/apartmentmanagement/config/WebSecurityConfig.java
+++ src/main/java/com/example/apartmentmanagement/config/WebSecurityConfig.java
@@ -29,11 +29,10 @@ public class WebSecurityConfig implements WebMvcConfigurer {
                 .csrf(csrf -> csrf.disable())
                 .cors(withDefaults())
                 .authorizeHttpRequests(auth -> auth
-
-                        .requestMatchers("/api/login", "/bill/**", "/apartment/**","/api/forgot_password","/api/reset_password",
-                                "/api/log_out", "/payment/process","/user/**", "/notification/**", "/api/reports/**",
+                        .requestMatchers("/api/**", "/bill/**", "/apartment/**",
+                                "/payment/process","/user/**", "/notification/**", "/api/reports/**",
                                 "/public/**","/api/replies/report/**", "/api/replies/report/**", "/api/replies/**",
-                                "/ws/**", "/chat/**", "/app/**")
+                                "/ws/**", "/chat/**", "/app/**", "/post/**")
                         .permitAll()
 
                         .anyRequest().authenticated()

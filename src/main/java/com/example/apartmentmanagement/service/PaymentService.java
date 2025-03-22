bbd687bfd1d1a216d4b9f7b8f43e053b129d9500
MiiNorin <hodangdxb@gmail.com>
2025-03-22 15:25:54 +0700

    feature: payment history

--- src/main/java/com/example/apartmentmanagement/service/PaymentService.java
+++ src/main/java/com/example/apartmentmanagement/service/PaymentService.java
@@ -1,7 +1,10 @@
 package com.example.apartmentmanagement.service;
 
+import com.example.apartmentmanagement.dto.PaymentHistoryResponseDTO;
 import com.example.apartmentmanagement.entities.Payment;
 
+import java.util.List;
+
 public interface PaymentService {
-    Payment processPayment(Long billId, String username);
+    List<PaymentHistoryResponseDTO> getPaymentHistory(Long userId, int month, int year);
 }

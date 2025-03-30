a574fbfd3414d5fe94aa1ee30a6d6ff70222bd20
MiiNorin <hodangdxb@gmail.com>
2025-03-30 23:52:55 +0700

    update: consumption

--- src/main/java/com/example/apartmentmanagement/repository/ConsumptionRepository.java
+++ src/main/java/com/example/apartmentmanagement/repository/ConsumptionRepository.java
@@ -1,7 +1,11 @@
 package com.example.apartmentmanagement.repository;
 
+import com.example.apartmentmanagement.entities.Apartment;
 import com.example.apartmentmanagement.entities.Consumption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ConsumptionRepository extends JpaRepository<Consumption, Long> {
+    List<Consumption> findByApartment(Apartment apartment);
 }
